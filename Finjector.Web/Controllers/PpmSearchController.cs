de4cddd98cc8c5bcf168eb0fd79dd93fdc70b1c5
Scott Kirkland <srkirkland@gmail.com>
2022-10-05 14:16:56 -0700

    implement most of search

--- Finjector.Web/Controllers/PpmSearchController.cs
+++ Finjector.Web/Controllers/PpmSearchController.cs
@@ -41,4 +41,60 @@ public class PpmSearchController : ControllerBase
 
         return Ok(projects.DistinctBy(p => p.Code));
     }
+
+
+    // TODO: task is special and will need to take the project number into account eventually
+    // query will search task name
+    [HttpGet("task")]
+    public async Task<IActionResult> Task(string query)
+    {
+        var projectId = "300000008444977";
+        var filter = new PpmTaskFilterInput { TaskNumber = new StringFilterInput { Contains = query }, ProjectId = new StringFilterInput { Eq = projectId } };
+
+        var result = await _apiClient.PpmTaskSearch.ExecuteAsync(filter);
+
+        var data = result.ReadData();
+
+        var tasks = data.PpmTaskSearch.Data.Select(d => new SearchResult(d.TaskNumber, d.Name));
+
+        return Ok(tasks.DistinctBy(p => p.Code));
+    }
+
+    [HttpGet("organization")]
+    public async Task<IActionResult> Organization(string query)
+    {
+        var filter = new PpmOrganizationFilterInput { Name = new StringFilterInput { Contains = query } };
+
+        var result = await _apiClient.PpmOrganizationSearch.ExecuteAsync(filter, query);
+
+        var data = result.ReadData();
+
+        var orgs = data.PpmOrganizationSearch.Data.Select(d => new SearchResult(d.Code, d.Name));
+
+        if (data.PpmOrganization != null)
+        {
+            orgs = orgs.Append(new SearchResult(data.PpmOrganization.Code, data.PpmOrganization.Name));
+        }
+
+        return Ok(orgs.DistinctBy(p => p.Code));
+    }
+
+    [HttpGet("expenditureType")]
+    public async Task<IActionResult> ExpenditureType(string query)
+    {
+        var filter = new PpmExpenditureTypeFilterInput { Name = new StringFilterInput { Contains = query } };
+
+        var result = await _apiClient.PpmExpenditureTypeSearch.ExecuteAsync(filter, query);
+
+        var data = result.ReadData();
+
+        var projects = data.PpmExpenditureTypeSearch.Data.Select(d => new SearchResult(d.Code, d.Name));
+
+        if (data.PpmExpenditureTypeByCode != null)
+        {
+            projects = projects.Append(new SearchResult(data.PpmExpenditureTypeByCode.Code, data.PpmExpenditureTypeByCode.Name));
+        }
+
+        return Ok(projects.DistinctBy(p => p.Code));
+    }
 }
