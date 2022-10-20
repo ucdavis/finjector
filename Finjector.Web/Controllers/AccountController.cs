4c595f7118d97fe6819205d5df7f71f16d3e4b24
Scott Kirkland <srkirkland@gmail.com>
2022-10-20 10:38:46 -0700

    user is queried before app renders
    if 401, login, otherwise return user info.  The info is actually ignored (for now, maybe header eventually?) but this serves as a login check.

--- Finjector.Web/Controllers/AccountController.cs
+++ Finjector.Web/Controllers/AccountController.cs
@@ -6,24 +6,9 @@ namespace Finjector.Web.Controllers;
 
 public class AccountController : Controller
 {
-    private readonly IHttpContextAccessor httpContextAccessor;
-
-    public AccountController(IHttpContextAccessor _httpContextAccessor)
-    {
-        this.httpContextAccessor = _httpContextAccessor;
-    }
-
     [Authorize(AuthenticationSchemes = OpenIdConnectDefaults.AuthenticationScheme)] // trigger authentication
     public IActionResult Login(string? returnUrl)
     {
-        // attempt to add custom cookie
-        this.httpContextAccessor.HttpContext?.Response.Cookies.Append("ASPNET_AUTH_SESSION", "true", new CookieOptions
-        {
-            HttpOnly = false, // need client to read it
-            Secure = true,
-            SameSite = SameSiteMode.Lax
-        });
-
         // redirect to return url if it exists, otherwise /
         return Redirect(returnUrl ?? "/");
     }
