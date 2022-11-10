144ba6df6fa22911672462612c0450ae886e8fd2
Cal <cydoval@mac.com>
2022-11-10 11:12:03 -0700

    new destination

--- Finjector.Web/ClientApp/src/shared/Header.tsx
+++ Finjector.Web/ClientApp/src/shared/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => (
   <div className="header">
     <div className="container">
       <div className="row">
-        <Link to="/landing">
+        <Link to="/">
           <img src="/media/coa-logo.svg" alt="" />
         </Link>
       </div>
