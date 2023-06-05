8c4afa9d0ef1f3968c5a1d3ddb0d055fc54f2a19
Scott Kirkland <srkirkland@gmail.com>
2023-06-05 15:12:09 -0700

    fix app tests too  just for fun

--- Finjector.Web/ClientApp/src/App.test.tsx
+++ Finjector.Web/ClientApp/src/App.test.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import App from "./App";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { fakeCharts } from "./util/mockData";
+import { cleanupApiMocks, mockApi } from "./util/test";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterAll(() => {
+  cleanupApiMocks();
 });
+
+const mockSavedChartsCall = () => {
+  mockApi().get("/api/charts/all").reply(200, fakeCharts);
+};
+
+const mockUserInfoCall = () => {
+  mockApi().get("/api/user/info").reply(200, {});
+};
+
+describe("App", () => {
+  it("shows landing page once user logged in", async () => {
+    mockUserInfoCall();
+    mockSavedChartsCall();
+
+    render(wrappedView());
+
+    // should see landing page
+    await waitFor(() => {
+      expect(
+        screen.getByText("Create new chart from scratch")
+      ).toBeInTheDocument();
+    });
+  });
+});
+
+const wrappedView = () => (
+  <QueryClientProvider client={new QueryClient()}>
+    <App />
+  </QueryClientProvider>
+);
