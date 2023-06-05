597b975d7ccdd69d3312ff6618cac95eef47429d
Scott Kirkland <srkirkland@gmail.com>
2023-06-05 14:55:37 -0700

    test of landing page
    an example of testing strategy using react-testing-lib and nock for mocking api.

---
+++ Finjector.Web/ClientApp/src/pages/Landing.test.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import { cleanupApiMocks, mockApi } from "../util/test";
+import { fakeCharts } from "../util/mockData";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+import Landing from "./Landing";
+import { render, screen, waitFor } from "@testing-library/react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+afterAll(() => {
+  cleanupApiMocks();
+});
+
+const mockSavedChartsCall = () => {
+  mockApi().get("/api/charts/all").reply(200, fakeCharts);
+};
+
+// test main landing page
+describe("Landing", () => {
+  it("renders", () => {
+    // setup mock
+    mockSavedChartsCall();
+
+    // render component
+    render(wrappedView());
+
+    // should see the create new chart buttons
+    expect(screen.getByRole("link", { name: /create new chart from scratch/i })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: /create new chart from paste/i })).toBeInTheDocument();
+  });
+
+  it("loads saved charts", async () => {
+    // setup mock
+    mockSavedChartsCall();
+
+    // render component
+    render(wrappedView());
+
+    // should see list of saved charts (3 from our mock data)
+    await waitFor(() => {
+      expect(screen.getByText("Chart 1")).toBeInTheDocument();
+    });
+  });
+
+});
+
+const wrappedView = () => (
+  <QueryClientProvider client={new QueryClient()}>
+    <MemoryRouter initialEntries={[`/`]}>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </MemoryRouter>
+  </QueryClientProvider>
+);
