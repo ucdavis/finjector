33346b60da178c7c51abf24de3666860e9186921
Jason Sylvestre <jsylvestre@ucdavis.edu>
2023-11-02 12:33:05 -0700

    c/p

--- Finjector.Core/CreateMigration.sh
+++ Finjector.Core/CreateMigration.sh
@@ -1,6 +1,6 @@
 ﻿[ "$#" -eq 1 ] || { echo "1 argument required, $# provided. Useage: sh CreateMigration <MigrationName>"; exit 1; }
 
 dotnet ef migrations add $1 --context AppDbContextSqlServer --output-dir Migrations --startup-project ../Finjector.Web/Finjector.Web.csproj -- --provider SqlServer
-# usage from PM console in the Hippo.Core directory: ./CreateMigration.sh <MigrationName>
+# usage from PM console in the Finjector.Core directory: ./CreateMigration.sh <MigrationName>
 
 echo 'All done';
