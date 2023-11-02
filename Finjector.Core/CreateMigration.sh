5332f86f1814b38f0c2f74ce810a6478e9be8994
Jason Sylvestre <jsylvestre@ucdavis.edu>
2023-11-02 11:00:24 -0700

    Migration stuff
    Only sql server for now

---
+++ Finjector.Core/CreateMigration.sh
@@ -0,0 +1,7 @@
+﻿[ "$#" -eq 1 ] || { echo "1 argument required, $# provided. Useage: sh CreateMigration <MigrationName>"; exit 1; }
+
+#dotnet ef migrations add $1 --context AppDbContextSqlite --output-dir Migrations/Sqlite --startup-project ../Finjector.Web/Finjector.Web.csproj -- --provider Sqlite
+dotnet ef migrations add $1 --context AppDbContextSqlServer --output-dir Migrations/SqlServer --startup-project ../Finjector.Web/Finjector.Web.csproj -- --provider SqlServer
+# usage from PM console in the Hippo.Core directory: ./CreateMigration.sh <MigrationName>
+
+echo 'All done';
---
+++ Finjector.Core/ExecuteMigration.sh
@@ -0,0 +1,5 @@
+﻿dotnet ef database update --startup-project ../Finjector.Web/Finjector.Web.csproj --context AppDbContextSqlServer
+# dotnet ef database update --startup-project ../Finjector.Web/Finjector.Web.csproj --context AppDbContextSqlite
+# usage from PM console in the Finjector.Core directory: ./ExecuteMigration.sh
+
+echo 'All done';
