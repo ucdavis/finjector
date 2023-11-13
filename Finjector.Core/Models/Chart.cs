7991bcb11643c9025393a276102e7cccf33adb8e
Jason Sylvestre <jsylvestre@ucdavis.edu>
2023-11-13 12:14:01 -0800

    Chart Model update for SQL

--- Finjector.Core/Models/Chart.cs
+++ Finjector.Core/Models/Chart.cs
@@ -7,15 +7,20 @@ namespace Finjector.Core.Models;
 public class Chart
 {
     [StringLength(36)]
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-    [StringLength(10)]
-    public string IamId { get; set; } = "";
+    public string Id { get; set; } = Guid.NewGuid().ToString();  //After move to SQL, this will be an int
+    [StringLength(10)] 
+    public string IamId { get; set; } = ""; //This will not matter once these have been migrated to SQL
     [StringLength(128)]
     public string SegmentString { get; set; } = "";
     [StringLength(200)]
     public string DisplayName { get; set; } = "";
     [StringLength(3)]
     public string ChartType { get; set; } = "";
+
+    public int FolderId { get; set; } = 0;
+    public string FolderName { get; set; } = "";
+    public int TeamId { get; set; } = 0;
+    public string TeamName { get; set; } = "";
 }
 
 
