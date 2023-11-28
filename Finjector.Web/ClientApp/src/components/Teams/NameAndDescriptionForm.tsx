203655a509071ff9c8901b9ccd00fa6c3d08ccd8
Scott Kirkland <srkirkland@gmail.com>
2023-11-28 11:22:11 -0800

    extracting out the shared form

---
+++ Finjector.Web/ClientApp/src/components/Teams/NameAndDescriptionForm.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+export interface NameAndDescriptionFormProps {
+  name: string;
+  description?: string;
+}
+
+interface Props {
+  initialValues?: NameAndDescriptionFormProps;
+  onSubmit: (formData: NameAndDescriptionFormProps) => void;
+}
+
+export default function FormComponent(props: Props) {
+  const { initialValues = { name: "", description: "" }, onSubmit } = props;
+  const [formData, setFormData] = useState(initialValues);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e: any) => {
+    console.log(e.target.value, e.target.name);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    setLoading(true);
+    await onSubmit(formData);
+    setLoading(false);
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name:
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+          maxLength={50}
+        />
+        <div className="invalid-feedback">Please provide a name.</div>
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">
+          Description:
+        </label>
+        <textarea
+          className="form-control"
+          id="description"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          maxLength={300}
+        />
+        <div className="invalid-feedback">Please provide a description.</div>
+      </div>
+
+      <button type="submit" className="btn btn-primary" disabled={loading}>
+        {loading ? "Creating..." : "Create"}
+      </button>
+    </form>
+  );
+}
--- Finjector.Web/ClientApp/src/pages/Teams/CreateTeam.tsx
+++ Finjector.Web/ClientApp/src/pages/Teams/CreateTeam.tsx
@@ -1,30 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { useCreateTeamMutation } from "../../queries/teamQueries";
 import { useNavigate } from "react-router-dom";
+import NameAndDescriptionForm, {
+  NameAndDescriptionFormProps,
+} from "../../components/Teams/NameAndDescriptionForm";
 
 const CreateTeam: React.FC = () => {
   const navigate = useNavigate();
 
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-
   const createTeamMutation = useCreateTeamMutation();
 
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
-  };
-
-  const handleDescriptionChange = (
-    event: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    setDescription(event.target.value);
-  };
-
-  const handleCreate = () => {
-    createTeamMutation.mutate(
+  const handleCreate = async (data: NameAndDescriptionFormProps) => {
+    await createTeamMutation.mutateAsync(
       {
-        name,
-        description,
+        name: data.name,
+        description: data.description,
         id: 0,
         isPersonal: false,
       },
@@ -41,45 +31,7 @@ const CreateTeam: React.FC = () => {
 
   return (
     <div>
-      <form className="needs-validation" noValidate>
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name:
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            value={name}
-            onChange={handleNameChange}
-            required
-            maxLength={50}
-          />
-          <div className="invalid-feedback">Please provide a name.</div>
-        </div>
-
-        <div className="mb-3">
-          <label htmlFor="description" className="form-label">
-            Description:
-          </label>
-          <textarea
-            className="form-control"
-            id="description"
-            value={description}
-            onChange={handleDescriptionChange}
-            maxLength={300}
-          />
-          <div className="invalid-feedback">Please provide a description.</div>
-        </div>
-
-        <button
-          type="button"
-          className="btn btn-primary"
-          onClick={handleCreate}
-        >
-          Create
-        </button>
-      </form>
+      <NameAndDescriptionForm onSubmit={handleCreate} />
     </div>
   );
 };
