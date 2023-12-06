df1c0b02823387fe137a1317a9940cf411d89a3b
River Holstege <laholstege@ucdavis.edu>
2023-12-06 15:49:49 -0800

    cannot decide if huge or small brain activity

--- Finjector.Web/ClientApp/src/components/Entry/FolderSearch.tsx
+++ Finjector.Web/ClientApp/src/components/Entry/FolderSearch.tsx
@@ -15,16 +15,7 @@ import { Input, InputGroup, InputGroupText } from "reactstrap";
 
 const FolderSearch: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const [selectedFolder, setSelectedFolder] = useState<Folder>({
-    id: 0,
-    name: "Default",
-    teamName: "Personal",
-    teamId: 0,
-    description: "",
-    myFolderPermissions: [],
-    myTeamPermissions: [],
-    coas: [],
-  });
+  const [selectedFolder, setSelectedFolder] = useState<Folder>();
 
   const { data, isFetching } = useSearchFolders(searchTerm);
 
@@ -42,79 +33,67 @@ const FolderSearch: React.FC = () => {
   return (
     <div className="mb-3 col-sm-6">
       <label className="form-label">Folder Search</label>
-      <AsyncTypeahead
-        id={"typeahead-folder-search"}
-        filterBy={() => true} // don't filter since we're doing it on the server
-        isLoading={isFetching}
-        labelKey="name"
-        //   minLength={minQueryLength}
-        onSearch={() => {}}
-        onInputChange={handleInputChange}
-        defaultInputValue="Default"
-        onChange={handleSelected}
-        useCache={false}
-        options={data || []} // data
-        placeholder={`Search for Folder...`}
-        renderMenu={(
-          results,
-          {
-            newSelectionPrefix, // we dont want to pass this to the menu or it gets mad
-            paginationText,
-            renderMenuItemChildren,
-            ...menuProps
-          }
-        ) => {
-          let index = 0;
-          const teamFolders = groupBy(results, "teamName");
-          const items = Object.keys(teamFolders)
-            .sort()
-            .map((teamName) => (
-              <Fragment key={teamName}>
-                {index !== 0 && <Menu.Divider />}
-                <Menu.Header>
-                  <h5>
-                    Team:{" "}
-                    <Highlighter search={searchTerm}>{teamName}</Highlighter>
-                  </h5>
-                </Menu.Header>
-                {teamFolders[teamName].map((folder: any) => {
-                  const item = (
-                    <MenuItem key={index} option={folder} position={index}>
-                      <FontAwesomeIcon icon={faFolder} />{" "}
-                      <span>
-                        <Highlighter search={searchTerm}>
-                          {folder.name}
-                        </Highlighter>
-                      </span>
-                    </MenuItem>
-                  );
+      <InputGroup>
+        <InputGroupText>
+          {selectedFolder?.teamName ?? "Personal"}
+        </InputGroupText>
+        <AsyncTypeahead
+          id={"typeahead-folder-search"}
+          filterBy={() => true} // don't filter since we're doing it on the server
+          isLoading={isFetching}
+          labelKey="name"
+          //   minLength={minQueryLength}
+          onSearch={() => {}}
+          onInputChange={handleInputChange}
+          defaultInputValue="Default"
+          onChange={handleSelected}
+          useCache={false}
+          options={data || []} // data
+          placeholder={`Search for Folder...`}
+          renderMenu={(
+            results,
+            {
+              newSelectionPrefix, // we dont want to pass this to the menu or it gets mad
+              paginationText,
+              renderMenuItemChildren,
+              ...menuProps
+            }
+          ) => {
+            let index = 0;
+            const teamFolders = groupBy(results, "teamName");
+            const items = Object.keys(teamFolders)
+              .sort()
+              .map((teamName) => (
+                <Fragment key={teamName}>
+                  {index !== 0 && <Menu.Divider />}
+                  <Menu.Header>
+                    <h5>
+                      Team:{" "}
+                      <Highlighter search={searchTerm}>{teamName}</Highlighter>
+                    </h5>
+                  </Menu.Header>
+                  {teamFolders[teamName].map((folder: any) => {
+                    const item = (
+                      <MenuItem key={index} option={folder} position={index}>
+                        <FontAwesomeIcon icon={faFolder} />{" "}
+                        <span>
+                          <Highlighter search={searchTerm}>
+                            {folder.name}
+                          </Highlighter>
+                        </span>
+                      </MenuItem>
+                    );
 
-                  index += 1;
-                  return item;
-                })}
-              </Fragment>
-            ));
+                    index += 1;
+                    return item;
+                  })}
+                </Fragment>
+              ));
 
-          return <Menu {...menuProps}>{items}</Menu>;
-        }}
-        renderInput={(inputProps, props) => {
-          const { inputRef, type, referenceElementRef, ...rest } = inputProps; // have to separate these out to get the types to play nice
-          const selected: any = props.selected[0]; // will always be a single selection
-          return (
-            <Hint>
-              <InputGroup>
-                <InputGroupText>
-                  {selected?.teamName ?? "Personal"}
-                </InputGroupText>
-                <Input {...rest} type="search" innerRef={inputRef}></Input>
-              </InputGroup>
-            </Hint>
-          );
-        }}
-      />
-      <div className="form-text">
-        Team: {selectedFolder?.teamName}, Folder: {selectedFolder?.name}
-      </div>
+            return <Menu {...menuProps}>{items}</Menu>;
+          }}
+        />
+      </InputGroup>
     </div>
   );
 };
