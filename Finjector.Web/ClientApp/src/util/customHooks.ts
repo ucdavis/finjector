b183d17b07abe6a92b1cb8e96aee63b8a07bd3e8
Scott Kirkland <srkirkland@gmail.com>
2023-12-11 09:57:26 -0800

    dynamically chose click destination based on popup status

---
+++ Finjector.Web/ClientApp/src/util/customHooks.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+const usePopupStatus = (): boolean => {
+  const [isWindowOpenerPopulated, setIsWindowOpenerPopulated] = useState(false);
+
+  useEffect(() => {
+    setIsWindowOpenerPopulated(!!window.opener);
+  }, []);
+
+  return isWindowOpenerPopulated;
+};
+
+export default usePopupStatus;
