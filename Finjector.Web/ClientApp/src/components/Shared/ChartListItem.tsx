c63967269167db612a51250a24d097d6afa19c93
Scott Kirkland <srkirkland@gmail.com>
2023-12-11 14:41:10 -0800

    remove the nested <a> tags

--- Finjector.Web/ClientApp/src/components/Shared/ChartListItem.tsx
+++ Finjector.Web/ClientApp/src/components/Shared/ChartListItem.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Coa, ChartType } from "../../types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBolt, faScroll } from "@fortawesome/free-solid-svg-icons";
@@ -12,51 +12,52 @@ interface Props {
 const ChartListItem = ({ chart }: Props) => {
   const isInPopup = usePopupStatus();
 
-  const destination = isInPopup ? "/selected" : "/details";
+  const navigate = useNavigate();
+
+  const onChartClick = () => {
+    const destination = isInPopup ? "/selected" : "/details";
+    navigate(`${destination}/${chart.id}/${chart.segmentString}`);
+  };
 
   return (
-    <Link
-      className="chartstring-link"
-      to={`${destination}/${chart.id}/${chart.segmentString}`}
+    <li
+      className={`chartstring-row chartstring-link ${
+        chart.chartType === ChartType.PPM ? "is-ppm" : "is-gl"
+      } d-flex justify-content-between align-items-center saved-list-item`}
+      key={chart.id}
+      onClick={onChartClick}
     >
-      <li
-        className={`chartstring-row ${
-          chart.chartType === ChartType.PPM ? "is-ppm" : "is-gl"
-        } d-flex justify-content-between align-items-center saved-list-item`}
-        key={chart.id}
-      >
-        <div className="col-9 ms-2 me-auto">
-          <div className="chartstring-type">
-            <span>{chart.chartType}</span>
-          </div>
-          <div className="fw-bold "> {chart.name}</div>
-          <span style={{ wordWrap: "break-word" }}>
-            <CopyToClipboardHover
-              value={chart.segmentString}
-              id={`chart-list-${chart.id}`}
-            >
-              {chart.segmentString}
-            </CopyToClipboardHover>
-          </span>
+      <div className="col-9 ms-2 me-auto">
+        <div className="chartstring-type">
+          <span>{chart.chartType}</span>
         </div>
-        <div className="col-3 text-end">
-          <Link
-            to={`/details/${chart.id}/${chart.segmentString}`}
-            className="btn btn-link"
+        <div className="fw-bold "> {chart.name}</div>
+        <span style={{ wordWrap: "break-word" }}>
+          <CopyToClipboardHover
+            value={chart.segmentString}
+            id={`chart-list-${chart.id}`}
           >
-            <FontAwesomeIcon icon={faScroll} />
-            Details
-          </Link>
-          <Link
-            to={`/selected/${chart.id}/${chart.segmentString}`}
-            className="btn btn-link"
-          >
-            <FontAwesomeIcon icon={faBolt} />
-            Use
-          </Link>
-        </div>
-      </li>
-    </Link>
+            {chart.segmentString}
+          </CopyToClipboardHover>
+        </span>
+      </div>
+      <div className="col-3 text-end">
+        <Link
+          to={`/details/${chart.id}/${chart.segmentString}`}
+          className="btn btn-link"
+        >
+          <FontAwesomeIcon icon={faScroll} />
+          Details
+        </Link>
+        <Link
+          to={`/selected/${chart.id}/${chart.segmentString}`}
+          className="btn btn-link"
+        >
+          <FontAwesomeIcon icon={faBolt} />
+          Use
+        </Link>
+      </div>
+    </li>
   );
 };
 
