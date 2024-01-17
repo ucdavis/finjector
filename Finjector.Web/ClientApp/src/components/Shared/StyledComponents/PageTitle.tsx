0224eccbc138e13b6aa63beecf3e0309e386d521
River Holstege <laholstege@ucdavis.edu>
2024-01-17 13:57:20 -0800

    typo

--- Finjector.Web/ClientApp/src/components/Shared/StyledComponents/PageTitle.tsx
+++ Finjector.Web/ClientApp/src/components/Shared/StyledComponents/PageTitle.tsx
@@ -12,7 +12,7 @@ const PageTitle: React.FC<PageTitleProps> = (
   let divClassName = `page-title mb-3 ${className ?? ""}`;
   if (Array.isArray(children) && children.length > 1) {
     // this will not be applied if children is a single element (such as <h1>, <div>, <>) or a string
-    // but if we have multiple elements, put them in a row and apply to correct styles
+    // but if we have multiple elements, put them in a row and apply the correct styles
     divClassName += " pb-2 row justify-content-between align-items-center";
   }
   return (
