da208776537789fcd7baf7b7e17dbdeaf0c08b1e
Scott Kirkland <srkirkland@gmail.com>
2024-01-18 11:07:17 -0800

    bulk import coa passing

--- Finjector.Web/ClientApp/src/pages/Import.tsx
+++ Finjector.Web/ClientApp/src/pages/Import.tsx
@@ -1,18 +1,21 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { useGetSavedCharts } from "../queries/storedChartQueries";
 import ImportListTeamRow from "../components/Import/ImportListTeamRow";
-import { TeamGroupedCoas } from "../types";
+import { Coa, TeamGroupedCoas } from "../types";
+import usePopupStatus from "../util/customHooks";
 
 // Main landing screen for popup
 
 const Import = () => {
   const [search, setSearch] = React.useState("");
 
+  const isPopup = usePopupStatus();
+
   const teamGroupsQuery = useGetSavedCharts();
 
   const teamGroups = teamGroupsQuery.data;
 
-  const filteredTeamGroups = useMemo(() => {
+  const filteredTeamGroups = React.useMemo(() => {
     if (!teamGroups) return [];
 
     if (!search) return teamGroups;
@@ -39,6 +42,37 @@ const Import = () => {
     });
   }, [teamGroups, search]);
 
+  const onImport = React.useCallback((chartStrings: Coa[]) => {
+    // get only the name and chart strings from each chart
+    const importData = chartStrings.map((chart) => ({
+      name: chart.name,
+      chartString: chart.segmentString,
+    }));
+
+    if (window.opener) {
+      window.opener.postMessage(
+        {
+          source: "finjector",
+          operation: "bulk-import",
+          status: "success",
+          data: importData,
+        },
+        "*" // send to all, we don't know the origin
+      );
+      // message sent, close the window
+      window.close();
+    }
+  }, []);
+
+  if (!isPopup) {
+    return (
+      <div>
+        <p>Import is only intended for use in a popup window.</p>
+        <a href="/">Back to homepage</a>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div className="page-title pb-2 mb-3 row justify-content-between align-items-center">
@@ -58,7 +92,11 @@ const Import = () => {
       <ul className="list-group">
         {filteredTeamGroups?.map((teamGroup) => {
           return (
-            <ImportListTeamRow key={teamGroup.team.id} teamGroup={teamGroup} />
+            <ImportListTeamRow
+              key={teamGroup.team.id}
+              teamGroup={teamGroup}
+              onImport={onImport}
+            />
           );
         })}
       </ul>
