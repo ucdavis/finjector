7753c946caf294445e6046b79449c5d84672be06
Scott Kirkland <srkirkland@gmail.com>
2024-01-18 10:36:57 -0800

    fix search for import

--- Finjector.Web/ClientApp/src/pages/Import.tsx
+++ Finjector.Web/ClientApp/src/pages/Import.tsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useGetSavedCharts } from "../queries/storedChartQueries";
 import ImportListTeamRow from "../components/Import/ImportListTeamRow";
+import { TeamGroupedCoas } from "../types";
 
 // Main landing screen for popup
 
 const Import = () => {
   const [search, setSearch] = React.useState("");
 
-  const teamGroups = useGetSavedCharts();
+  const teamGroupsQuery = useGetSavedCharts();
 
-  const filterLowercase = search.toLowerCase();
-  const filteredTeamsInfo = teamGroups.data?.filter((teamInfo) => {
-    return teamInfo.team.name.toLowerCase().includes(filterLowercase);
-  });
+  const teamGroups = teamGroupsQuery.data;
+
+  const filteredTeamGroups = useMemo(() => {
+    if (!teamGroups) return [];
+
+    if (!search) return teamGroups;
+
+    const searchLower = search.toLowerCase();
+
+    const teamGroupsClone: TeamGroupedCoas[] = JSON.parse(
+      JSON.stringify(teamGroups)
+    );
+
+    // filter out charts that don't match the search filter
+    return teamGroupsClone.filter((teamGroup) => {
+      // Check if team name matches the query
+      if (teamGroup.team.name.toLowerCase().includes(searchLower)) return true;
+
+      // Filter the folders within the team
+      teamGroup.folders = teamGroup.folders.filter((folder) => {
+        // Check if folder name matches the query
+        return folder.name.toLowerCase().includes(searchLower);
+      });
+
+      // Keep the team if it still has folders after filtering
+      return teamGroup.folders.length > 0;
+    });
+  }, [teamGroups, search]);
 
   return (
     <div>
@@ -31,7 +56,7 @@ const Import = () => {
         />
       </div>
       <ul className="list-group">
-        {filteredTeamsInfo?.map((teamGroup) => {
+        {filteredTeamGroups?.map((teamGroup) => {
           return (
             <ImportListTeamRow key={teamGroup.team.id} teamGroup={teamGroup} />
           );
