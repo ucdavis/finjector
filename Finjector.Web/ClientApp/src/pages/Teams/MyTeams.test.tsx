7cae2953749dc08eab5ffd64555f3041bfbeeb0f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-04-03 13:35:31 -0700

    WIP

--- Finjector.Web/ClientApp/src/pages/Teams/MyTeams.test.tsx
+++ Finjector.Web/ClientApp/src/pages/Teams/MyTeams.test.tsx
@@ -6,6 +6,7 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { describe, it, expect } from "vitest";
 import { server } from "../../../test/mocks/node";
 import MyTeams from "./MyTeams";
+import userEvent from "@testing-library/user-event";
 //import { fakeTeams, fakeFolders } from "../../../test/mocks/mockData";
 
 beforeAll(() => server.listen());
@@ -20,6 +21,28 @@ describe("MyTeams", () => {
     expect(
       screen.getByRole("link", { name: /Create New Team/i })
     ).toBeInTheDocument();
+  });
+
+  it("renders loading", async () => {
+    // render component
+    render(wrappedView());
+    expect(
+      screen.queryByText("Scribbling in Your Teams...")
+    ).toBeInTheDocument();
+    expect(screen.queryByText("Loading...")).toBeInTheDocument();
+
+    expect(
+      screen.getByRole("link", { name: /Create New Team/i })
+    ).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByText("My Teams")).toBeInTheDocument();
+    });
+
+    expect(
+      screen.queryByText("Scribbling in Your Teams...")
+    ).not.toBeInTheDocument();
+    expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
   });
 
   it("renders search box", async () => {
@@ -38,8 +61,6 @@ describe("MyTeams", () => {
     await waitFor(() => {
       expect(screen.getByText("My Teams")).toBeInTheDocument();
     });
-
-    console.log(screen.debug());
   });
 
   it("renders teams", async () => {
@@ -49,81 +70,39 @@ describe("MyTeams", () => {
     await waitFor(() => {
       expect(screen.getByText("Team 1")).toBeInTheDocument();
       expect(screen.getByText("Team 0")).toBeInTheDocument();
-      console.log(screen.debug(undefined, 20000));
-      //console.log(prettyDOM(document.body, 20000));
+      expect(screen.getAllByText("Go to Team").length).toBe(2);
+      //console.log(screen.debug(undefined, 20000));
     });
   });
 
-  //   it("renders error state", () => {
-  //     useGetMyTeams.mockReturnValue({
-  //       isLoading: false,
-  //       isError: true,
-  //       data: null,
-  //     });
+  it("renders teams links", async () => {
+    // render component
+    render(wrappedView());
 
-  //     render(
-  //       <MemoryRouter>
-  //         <MyTeams />
-  //       </MemoryRouter>
-  //     );
+    await waitFor(() => {
+      const teamLinks = screen.getAllByRole("link", { name: /Go to Team/i });
+      expect(teamLinks[0]).toHaveAttribute("href", "/teams/1");
+      expect(teamLinks[1]).toHaveAttribute("href", "/teams/0");
+    });
+  });
 
-  //     expect(screen.getByText("Error loading Teams")).toBeInTheDocument();
-  //   });
+  it("filters teams list", async () => {
+    const user = userEvent.setup();
+    // render component
+    render(wrappedView());
 
-  //   it("renders teams list", async () => {
-  //     const teams = [
-  //       { id: 1, name: "Team 1" },
-  //       { id: 2, name: "Team 2" },
-  //     ];
+    // Check for existing teams before filtering
+    await waitFor(() => {
+      expect(screen.getByText("Team 1")).toBeInTheDocument();
+      expect(screen.getByText("Team 0")).toBeInTheDocument();
+      expect(screen.getAllByText("Go to Team").length).toBe(2);
+    });
 
-  //     useGetMyTeams.mockReturnValue({
-  //       isLoading: false,
-  //       isError: false,
-  //       data: teams,
-  //     });
-
-  //     render(
-  //       <MemoryRouter>
-  //         <MyTeams />
-  //       </MemoryRouter>
-  //     );
-
-  //     await waitFor(() => {
-  //       expect(screen.getByText("My Teams")).toBeInTheDocument();
-  //       expect(screen.getByText("Team 1")).toBeInTheDocument();
-  //       expect(screen.getByText("Team 2")).toBeInTheDocument();
-  //     });
-  //   });
-
-  //   it("filters teams list", async () => {
-  //     const teams = [
-  //       { id: 1, name: "Team 1" },
-  //       { id: 2, name: "Team 2" },
-  //     ];
-
-  //     useGetMyTeams.mockReturnValue({
-  //       isLoading: false,
-  //       isError: false,
-  //       data: teams,
-  //     });
-
-  //     render(
-  //       <MemoryRouter>
-  //         <MyTeams />
-  //       </MemoryRouter>
-  //     );
-
-  //     const searchInput = screen.getByPlaceholderText("Search My Teams");
-
-  //     // Type "Team 1" in the search input
-  //     await waitFor(() => {
-  //       userEvent.type(searchInput, "Team 1");
-  //     });
-
-  //     // Only "Team 1" should be visible
-  //     expect(screen.getByText("Team 1")).toBeInTheDocument();
-  //     expect(screen.queryByText("Team 2")).not.toBeInTheDocument();
-  //   });
+    // search for a chart
+    const searchField = screen.getByRole("searchbox");
+    //type text into the search field
+    await user.type(searchField, "Team 0");
+  });
 });
 
 const wrappedView = () => (
