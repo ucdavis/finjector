f4a888d57056681d085fb1316ac771894bb4f06a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-04-22 11:12:33 -0700

    test: actions

--- Finjector.Web/ClientApp/src/pages/Teams/MyTeams.test.tsx
+++ Finjector.Web/ClientApp/src/pages/Teams/MyTeams.test.tsx
@@ -13,207 +13,252 @@ beforeEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe("MyTeams", () => {
-  it("renders", async () => {
-    // render component
-    render(wrappedView());
+  describe("renders", () => {
+    it("renders", async () => {
+      // render component
+      render(wrappedView());
 
-    expect(
-      screen.getByRole("link", { name: /Create New Team/i })
-    ).toBeInTheDocument();
+      expect(
+        screen.getByRole("link", { name: /Create New Team/i })
+      ).toBeInTheDocument();
+    });
+
+    it("renders loading", async () => {
+      // render component
+      render(wrappedView());
+      expect(
+        screen.queryByText("Scribbling in Your Teams...")
+      ).toBeInTheDocument();
+      expect(screen.queryByText("Loading...")).toBeInTheDocument();
+
+      expect(
+        screen.getByRole("link", { name: /Create New Team/i })
+      ).toBeInTheDocument();
+
+      await waitFor(() => {
+        expect(screen.getByText("My Teams")).toBeInTheDocument();
+      });
+
+      expect(
+        screen.queryByText("Scribbling in Your Teams...")
+      ).not.toBeInTheDocument();
+      expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
+    });
+
+    it("renders search box", async () => {
+      // render component
+      render(wrappedView());
+
+      const searchField = screen.getByRole("searchbox");
+      expect(searchField).toBeInTheDocument();
+      expect(searchField).toHaveAttribute("placeholder", "Search My Teams");
+    });
+
+    it("renders title", async () => {
+      // render component
+      render(wrappedView());
+
+      await waitFor(() => {
+        expect(screen.getByText("My Teams")).toBeInTheDocument();
+      });
+    });
+
+    it("renders teams", async () => {
+      // render component
+      render(wrappedView());
+
+      await waitFor(() => {
+        expect(screen.getByText("Team 1")).toBeInTheDocument();
+        expect(screen.getByText("Team 0")).toBeInTheDocument();
+        expect(screen.getAllByText("Go to Team").length).toBe(2);
+        //console.log(screen.debug(undefined, 20000));
+      });
+    });
+
+    it("renders teams links", async () => {
+      // render component
+      render(wrappedView());
+
+      await waitFor(() => {
+        const teamLinks = screen.getAllByRole("link", { name: /Go to Team/i });
+        expect(teamLinks[0]).toHaveAttribute("href", "/teams/1");
+        expect(teamLinks[1]).toHaveAttribute("href", "/teams/0");
+      });
+    });
+
+    it("renders teams descriptions", async () => {
+      // render component
+      render(wrappedView());
+
+      await waitFor(() => {
+        expect(screen.getByText("Team 1 description")).toBeInTheDocument();
+        expect(screen.getByText("Team 0 description")).toBeInTheDocument();
+      });
+    });
+
+    it("filters teams list 1", async () => {
+      const user = userEvent.setup();
+      // render component
+      render(wrappedView());
+
+      // Check for existing teams before filtering
+      await waitFor(() => {
+        expect(screen.getByText("Team 1")).toBeInTheDocument();
+        expect(screen.getByText("Team 0")).toBeInTheDocument();
+        expect(screen.getAllByText("Go to Team").length).toBe(2);
+      });
+
+      // search for a chart
+      const searchField = screen.getByRole("searchbox");
+      //type text into the search field
+      await user.type(searchField, "Team 0");
+
+      await waitFor(() => {
+        expect(screen.getByText("Team 0")).toBeInTheDocument();
+        expect(screen.getByText("Team 0 description")).toBeInTheDocument();
+        expect(screen.getAllByText("Go to Team").length).toBe(1);
+        expect(screen.queryByText("Team 1")).not.toBeInTheDocument();
+        expect(
+          screen.queryByText("Team 1 description")
+        ).not.toBeInTheDocument();
+      });
+    });
+    it("filters teams list 2", async () => {
+      const user = userEvent.setup();
+      // render component
+      render(wrappedView());
+
+      // Check for existing teams before filtering
+      await waitFor(() => {
+        expect(screen.getByText("Team 1")).toBeInTheDocument();
+        expect(screen.getByText("Team 0")).toBeInTheDocument();
+        expect(screen.getAllByText("Go to Team").length).toBe(2);
+      });
+
+      // search for a chart
+      const searchField = screen.getByRole("searchbox");
+      //type text into the search field
+      await user.type(searchField, "0");
+
+      await waitFor(() => {
+        expect(screen.getByText("Team 0")).toBeInTheDocument();
+        expect(screen.getAllByText("Go to Team").length).toBe(1);
+        expect(screen.queryByText("Team 1")).not.toBeInTheDocument();
+      });
+    });
+
+    it("filters teams list 3 (no Match)", async () => {
+      const user = userEvent.setup();
+      // render component
+      render(wrappedView());
+
+      // Check for existing teams before filtering
+      await waitFor(() => {
+        expect(screen.getByText("Team 1")).toBeInTheDocument();
+        expect(screen.getByText("Team 0")).toBeInTheDocument();
+        expect(screen.getAllByText("Go to Team").length).toBe(2);
+      });
+
+      // search for a chart
+      const searchField = screen.getByRole("searchbox");
+      //type text into the search field
+      await user.type(searchField, "Te 0");
+
+      await waitFor(() => {
+        expect(screen.queryAllByText("Go to Team").length).toBe(0);
+        expect(screen.queryByText("Team 0")).not.toBeInTheDocument();
+        expect(screen.queryByText("Team 1")).not.toBeInTheDocument();
+      });
+    });
+
+    it("filters teams list 4", async () => {
+      const user = userEvent.setup();
+      // render component
+      render(wrappedView());
+
+      // Check for existing teams before filtering
+      await waitFor(() => {
+        expect(screen.getByText("Team 1")).toBeInTheDocument();
+        expect(screen.getByText("Team 0")).toBeInTheDocument();
+        expect(screen.getAllByText("Go to Team").length).toBe(2);
+      });
+
+      // search for a chart
+      const searchField = screen.getByRole("searchbox");
+      //type text into the search field
+      await user.type(searchField, "Team 1");
+
+      await waitFor(() => {
+        expect(screen.getByText("Team 1")).toBeInTheDocument();
+        expect(screen.getAllByText("Go to Team").length).toBe(1);
+        expect(screen.queryByText("Team 0")).not.toBeInTheDocument();
+      });
+    });
+
+    it("filters teams list 5 (no Match on description)", async () => {
+      const user = userEvent.setup();
+      // render component
+      render(wrappedView());
+
+      // Check for existing teams before filtering
+      await waitFor(() => {
+        expect(screen.getByText("Team 1")).toBeInTheDocument();
+        expect(screen.getByText("Team 1 description")).toBeInTheDocument();
+        expect(screen.getByText("Team 0")).toBeInTheDocument();
+        expect(screen.getAllByText("Go to Team").length).toBe(2);
+      });
+
+      // search for a chart
+      const searchField = screen.getByRole("searchbox");
+      //type text into the search field
+      await user.type(searchField, "description");
+
+      await waitFor(() => {
+        expect(screen.queryAllByText("Go to Team").length).toBe(0);
+        expect(screen.queryByText("Team 0")).not.toBeInTheDocument();
+        expect(screen.queryByText("Team 1")).not.toBeInTheDocument();
+      });
+    });
   });
+  describe("actions", () => {
+    it("clicks on create new team", async () => {
+      // render component
+      render(wrappedView());
 
-  it("renders loading", async () => {
-    // render component
-    render(wrappedView());
-    expect(
-      screen.queryByText("Scribbling in Your Teams...")
-    ).toBeInTheDocument();
-    expect(screen.queryByText("Loading...")).toBeInTheDocument();
+      await waitFor(() => {
+        const createTeamLink = screen.getByRole("link", {
+          name: /Create New Team/i,
+        });
 
-    expect(
-      screen.getByRole("link", { name: /Create New Team/i })
-    ).toBeInTheDocument();
-
-    await waitFor(() => {
-      expect(screen.getByText("My Teams")).toBeInTheDocument();
+        expect(createTeamLink).toBeInTheDocument();
+      });
+      const createTeamLink = screen.getByRole("link", {
+        name: /Create New Team/i,
+      });
+      await userEvent.click(createTeamLink);
+      await waitFor(() => {
+        expect(
+          screen.getByText("Yay redirected to create teams")
+        ).toBeInTheDocument();
+      });
     });
 
-    expect(
-      screen.queryByText("Scribbling in Your Teams...")
-    ).not.toBeInTheDocument();
-    expect(screen.queryByText("Loading...")).not.toBeInTheDocument();
-  });
+    it("goes to the expected team 1", async () => {
+      // render component
+      render(wrappedView());
 
-  it("renders search box", async () => {
-    // render component
-    render(wrappedView());
+      await waitFor(() => {
+        const teamLink = screen.getAllByRole("link", {
+          name: /Go to Team/i,
+        })[0];
+        expect(teamLink).toBeInTheDocument();
+      });
 
-    const searchField = screen.getByRole("searchbox");
-    expect(searchField).toBeInTheDocument();
-    expect(searchField).toHaveAttribute("placeholder", "Search My Teams");
-  });
-
-  it("renders title", async () => {
-    // render component
-    render(wrappedView());
-
-    await waitFor(() => {
-      expect(screen.getByText("My Teams")).toBeInTheDocument();
-    });
-  });
-
-  it("renders teams", async () => {
-    // render component
-    render(wrappedView());
-
-    await waitFor(() => {
-      expect(screen.getByText("Team 1")).toBeInTheDocument();
-      expect(screen.getByText("Team 0")).toBeInTheDocument();
-      expect(screen.getAllByText("Go to Team").length).toBe(2);
-      //console.log(screen.debug(undefined, 20000));
-    });
-  });
-
-  it("renders teams links", async () => {
-    // render component
-    render(wrappedView());
-
-    await waitFor(() => {
-      const teamLinks = screen.getAllByRole("link", { name: /Go to Team/i });
-      expect(teamLinks[0]).toHaveAttribute("href", "/teams/1");
-      expect(teamLinks[1]).toHaveAttribute("href", "/teams/0");
-    });
-  });
-
-  it("renders teams descriptions", async () => {
-    // render component
-    render(wrappedView());
-
-    await waitFor(() => {
-      expect(screen.getByText("Team 1 description")).toBeInTheDocument();
-      expect(screen.getByText("Team 0 description")).toBeInTheDocument();
-    });
-  });
-
-  it("filters teams list 1", async () => {
-    const user = userEvent.setup();
-    // render component
-    render(wrappedView());
-
-    // Check for existing teams before filtering
-    await waitFor(() => {
-      expect(screen.getByText("Team 1")).toBeInTheDocument();
-      expect(screen.getByText("Team 0")).toBeInTheDocument();
-      expect(screen.getAllByText("Go to Team").length).toBe(2);
-    });
-
-    // search for a chart
-    const searchField = screen.getByRole("searchbox");
-    //type text into the search field
-    await user.type(searchField, "Team 0");
-
-    await waitFor(() => {
-      expect(screen.getByText("Team 0")).toBeInTheDocument();
-      expect(screen.getByText("Team 0 description")).toBeInTheDocument();
-      expect(screen.getAllByText("Go to Team").length).toBe(1);
-      expect(screen.queryByText("Team 1")).not.toBeInTheDocument();
-      expect(screen.queryByText("Team 1 description")).not.toBeInTheDocument();
-    });
-  });
-  it("filters teams list 2", async () => {
-    const user = userEvent.setup();
-    // render component
-    render(wrappedView());
-
-    // Check for existing teams before filtering
-    await waitFor(() => {
-      expect(screen.getByText("Team 1")).toBeInTheDocument();
-      expect(screen.getByText("Team 0")).toBeInTheDocument();
-      expect(screen.getAllByText("Go to Team").length).toBe(2);
-    });
-
-    // search for a chart
-    const searchField = screen.getByRole("searchbox");
-    //type text into the search field
-    await user.type(searchField, "0");
-
-    await waitFor(() => {
-      expect(screen.getByText("Team 0")).toBeInTheDocument();
-      expect(screen.getAllByText("Go to Team").length).toBe(1);
-      expect(screen.queryByText("Team 1")).not.toBeInTheDocument();
-    });
-  });
-
-  it("filters teams list 3 (no Match)", async () => {
-    const user = userEvent.setup();
-    // render component
-    render(wrappedView());
-
-    // Check for existing teams before filtering
-    await waitFor(() => {
-      expect(screen.getByText("Team 1")).toBeInTheDocument();
-      expect(screen.getByText("Team 0")).toBeInTheDocument();
-      expect(screen.getAllByText("Go to Team").length).toBe(2);
-    });
-
-    // search for a chart
-    const searchField = screen.getByRole("searchbox");
-    //type text into the search field
-    await user.type(searchField, "Te 0");
-
-    await waitFor(() => {
-      expect(screen.queryAllByText("Go to Team").length).toBe(0);
-      expect(screen.queryByText("Team 0")).not.toBeInTheDocument();
-      expect(screen.queryByText("Team 1")).not.toBeInTheDocument();
-    });
-  });
-
-  it("filters teams list 4", async () => {
-    const user = userEvent.setup();
-    // render component
-    render(wrappedView());
-
-    // Check for existing teams before filtering
-    await waitFor(() => {
-      expect(screen.getByText("Team 1")).toBeInTheDocument();
-      expect(screen.getByText("Team 0")).toBeInTheDocument();
-      expect(screen.getAllByText("Go to Team").length).toBe(2);
-    });
-
-    // search for a chart
-    const searchField = screen.getByRole("searchbox");
-    //type text into the search field
-    await user.type(searchField, "Team 1");
-
-    await waitFor(() => {
-      expect(screen.getByText("Team 1")).toBeInTheDocument();
-      expect(screen.getAllByText("Go to Team").length).toBe(1);
-      expect(screen.queryByText("Team 0")).not.toBeInTheDocument();
-    });
-  });
-
-  it("filters teams list 5 (no Match on description)", async () => {
-    const user = userEvent.setup();
-    // render component
-    render(wrappedView());
-
-    // Check for existing teams before filtering
-    await waitFor(() => {
-      expect(screen.getByText("Team 1")).toBeInTheDocument();
-      expect(screen.getByText("Team 1 description")).toBeInTheDocument();
-      expect(screen.getByText("Team 0")).toBeInTheDocument();
-      expect(screen.getAllByText("Go to Team").length).toBe(2);
-    });
-
-    // search for a chart
-    const searchField = screen.getByRole("searchbox");
-    //type text into the search field
-    await user.type(searchField, "description");
-
-    await waitFor(() => {
-      expect(screen.queryAllByText("Go to Team").length).toBe(0);
-      expect(screen.queryByText("Team 0")).not.toBeInTheDocument();
-      expect(screen.queryByText("Team 1")).not.toBeInTheDocument();
+      const teamLink = screen.getAllByRole("link", { name: /Go to Team/i })[0];
+      await userEvent.click(teamLink);
+      await waitFor(() => {
+        expect(screen.getByText("Yay redirected to team")).toBeInTheDocument();
+      });
     });
   });
 });
@@ -223,6 +268,14 @@ const wrappedView = () => (
     <MemoryRouter initialEntries={[`/teams`]}>
       <Routes>
         <Route path="/teams" element={<MyTeams />} />
+        <Route
+          path="/teams/create"
+          element={<div>Yay redirected to create teams</div>}
+        />
+        <Route
+          path="/teams/:teamId"
+          element={<div>Yay redirected to team</div>}
+        />
       </Routes>
     </MemoryRouter>
   </QueryClientProvider>
