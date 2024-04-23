4618e75c1669f194d6912229c3754be66a558996
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-04-23 09:35:33 -0700

    Edit team action tests

--- Finjector.Web/ClientApp/src/pages/Teams/EditTeam.test.tsx
+++ Finjector.Web/ClientApp/src/pages/Teams/EditTeam.test.tsx
@@ -3,9 +3,11 @@ import React from "react";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { render, screen, waitFor } from "@testing-library/react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { server } from "../../../test/mocks/node";
 import EditTeam from "./EditTeam";
+import addFinToast from "../../components/Shared/LoadingAndErrors/FinToast";
+import userEvent from "@testing-library/user-event";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
@@ -22,57 +24,274 @@ describe("EditTeam tests", () => {
     });
   });
 
-  it("renders", async () => {
-    await waitFor(() => {
-      expect(screen.getByText("Edit Team")).toBeInTheDocument();
+  describe("render tests", () => {
+    it("renders", async () => {
+      await waitFor(() => {
+        expect(screen.getByText("Edit Team")).toBeInTheDocument();
+      });
     });
-  });
-  it("renders the team name", async () => {
-    await waitFor(() => {
-      expect(screen.getByText(`Team ${teamNumber}`)).toBeInTheDocument();
+    it("renders the team name", async () => {
+      await waitFor(() => {
+        expect(screen.getByText(`Team ${teamNumber}`)).toBeInTheDocument();
+      });
     });
-  });
-  it("renders the team description", async () => {
-    await waitFor(() => {
-      expect(
-        screen.getByText(`Team ${teamNumber} description`)
-      ).toBeInTheDocument();
+    it("renders the team description", async () => {
+      await waitFor(() => {
+        expect(
+          screen.getByText(`Team ${teamNumber} description`)
+        ).toBeInTheDocument();
+      });
     });
-  });
-  it("renders the name label", async () => {
-    await waitFor(() => {
-      expect(screen.getByText(/name:/i)).toBeInTheDocument();
+    it("renders the name label", async () => {
+      await waitFor(() => {
+        expect(screen.getByText(/name:/i)).toBeInTheDocument();
+      });
     });
-  });
-  it("renders the description label", async () => {
-    await waitFor(() => {
-      expect(screen.getByText(/description:/i)).toBeInTheDocument();
+    it("renders the description label", async () => {
+      await waitFor(() => {
+        expect(screen.getByText(/description:/i)).toBeInTheDocument();
+      });
     });
-  });
-  it("renders the name input field", () => {
-    const nameInput = screen.getByRole("textbox", {
-      name: /name:/i,
+    it("renders the name input field", () => {
+      const nameInput = screen.getByRole("textbox", {
+        name: /name:/i,
+      });
+
+      expect(nameInput).toBeInTheDocument();
+      expect(nameInput).toHaveValue(`Team ${teamNumber}`);
+      expect(nameInput).toBeEnabled();
+    });
+    it("renders the description input field", () => {
+      const descriptionInput = screen.getByRole("textbox", {
+        name: /description:/i,
+      });
+
+      expect(descriptionInput).toBeInTheDocument();
+      expect(descriptionInput).toHaveValue(`Team ${teamNumber} description`);
+      expect(descriptionInput).toBeEnabled();
     });
 
-    expect(nameInput).toBeInTheDocument();
-    expect(nameInput).toHaveValue(`Team ${teamNumber}`);
-    expect(nameInput).toBeEnabled();
-  });
-  it("renders the description input field", () => {
-    const descriptionInput = screen.getByRole("textbox", {
-      name: /description:/i,
+    it("renders the save button", () => {
+      const saveButton = screen.getByRole("button", { name: /save/i });
+
+      expect(saveButton).toBeInTheDocument();
+      expect(saveButton).toBeEnabled();
     });
-
-    expect(descriptionInput).toBeInTheDocument();
-    expect(descriptionInput).toHaveValue(`Team ${teamNumber} description`);
-    expect(descriptionInput).toBeEnabled();
   });
+  describe("action tests", () => {
+    beforeEach(() => {
+      vi.mock("../../components/Shared/LoadingAndErrors/FinToast", () => ({
+        default: vi.fn(),
+      }));
+    });
+    afterEach(() => {
+      vi.restoreAllMocks();
+    });
+    it("returns an error", async () => {
+      const user = userEvent.setup();
 
-  it("renders the save button", () => {
-    const saveButton = screen.getByRole("button", { name: /save/i });
+      //Verify fields are available and enabled
+      await waitFor(() => {
+        const nameInput = screen.getByRole("textbox", {
+          name: /name:/i,
+        });
+        expect(nameInput).toBeInTheDocument();
+        expect(nameInput).toBeEnabled();
+        expect(nameInput).toHaveValue(`Team ${teamNumber}`);
 
-    expect(saveButton).toBeInTheDocument();
-    expect(saveButton).toBeEnabled();
+        const descriptionInput = screen.getByRole("textbox", {
+          name: /description:/i,
+        });
+        expect(descriptionInput).toBeInTheDocument();
+        expect(descriptionInput).toBeEnabled();
+
+        const saveButton = screen.getByRole("button", {
+          name: /save changes/i,
+        });
+
+        expect(saveButton).toBeInTheDocument();
+        expect(saveButton).toBeEnabled();
+      });
+
+      const nameInput = screen.getByRole("textbox", {
+        name: /name:/i,
+      });
+      const saveButton = screen.getByRole("button", {
+        name: /save changes/i,
+      });
+      await user.clear(nameInput);
+      await user.type(nameInput, "TEST ERROR");
+
+      await waitFor(() => {
+        //expect name and description to be populated
+        expect(nameInput).toBeInTheDocument();
+        expect(nameInput).toHaveValue("TEST ERROR");
+      });
+
+      await user.click(saveButton);
+
+      await waitFor(() => {
+        expect(addFinToast).toBeCalledWith("error", "Error updating team.");
+        expect(
+          screen.queryByText("It Redirected Yay!")
+        ).not.toBeInTheDocument();
+      });
+    });
+    it("doesn't save if the team name is empty", async () => {
+      const user = userEvent.setup();
+
+      //Verify fields are available and enabled
+      await waitFor(() => {
+        const nameInput = screen.getByRole("textbox", {
+          name: /name:/i,
+        });
+        expect(nameInput).toBeInTheDocument();
+        expect(nameInput).toBeEnabled();
+        expect(nameInput).toHaveValue(`Team ${teamNumber}`);
+
+        const descriptionInput = screen.getByRole("textbox", {
+          name: /description:/i,
+        });
+        expect(descriptionInput).toBeInTheDocument();
+        expect(descriptionInput).toBeEnabled();
+
+        const saveButton = screen.getByRole("button", {
+          name: /save changes/i,
+        });
+
+        expect(saveButton).toBeInTheDocument();
+        expect(saveButton).toBeEnabled();
+      });
+
+      const nameInput = screen.getByRole("textbox", {
+        name: /name:/i,
+      });
+      const saveButton = screen.getByRole("button", {
+        name: /save changes/i,
+      });
+      await user.clear(nameInput);
+
+      await waitFor(() => {
+        //expect name and description to be populated
+        expect(nameInput).toBeInTheDocument();
+        expect(nameInput).toHaveValue("");
+      });
+
+      await user.click(saveButton);
+
+      await waitFor(() => {
+        expect(addFinToast).toBeCalledTimes(0);
+        expect(
+          screen.queryByText("It Redirected Yay!")
+        ).not.toBeInTheDocument();
+      });
+    });
+    it("saves the team", async () => {
+      const user = userEvent.setup();
+
+      //Verify fields are available and enabled
+      await waitFor(() => {
+        const nameInput = screen.getByRole("textbox", {
+          name: /name:/i,
+        });
+        expect(nameInput).toBeInTheDocument();
+        expect(nameInput).toBeEnabled();
+        expect(nameInput).toHaveValue(`Team ${teamNumber}`);
+
+        const descriptionInput = screen.getByRole("textbox", {
+          name: /description:/i,
+        });
+        expect(descriptionInput).toBeInTheDocument();
+        expect(descriptionInput).toBeEnabled();
+
+        const saveButton = screen.getByRole("button", {
+          name: /save changes/i,
+        });
+
+        expect(saveButton).toBeInTheDocument();
+        expect(saveButton).toBeEnabled();
+      });
+
+      const nameInput = screen.getByRole("textbox", {
+        name: /name:/i,
+      });
+      const saveButton = screen.getByRole("button", {
+        name: /save changes/i,
+      });
+      await user.clear(nameInput);
+      await user.type(nameInput, "Test Team");
+
+      await waitFor(() => {
+        //expect name and description to be populated
+        expect(nameInput).toBeInTheDocument();
+        expect(nameInput).toHaveValue("Test Team");
+      });
+
+      await user.click(saveButton);
+
+      await waitFor(() => {
+        expect(addFinToast).toBeCalledWith(
+          "success",
+          "Team updated successfully."
+        );
+        expect(screen.getByText("It Redirected Yay!")).toBeInTheDocument();
+      });
+    });
+    it("saves the team with an empty description", async () => {
+      const user = userEvent.setup();
+
+      //Verify fields are available and enabled
+      await waitFor(() => {
+        const nameInput = screen.getByRole("textbox", {
+          name: /name:/i,
+        });
+        expect(nameInput).toBeInTheDocument();
+        expect(nameInput).toBeEnabled();
+        expect(nameInput).toHaveValue(`Team ${teamNumber}`);
+
+        const descriptionInput = screen.getByRole("textbox", {
+          name: /description:/i,
+        });
+        expect(descriptionInput).toBeInTheDocument();
+        expect(descriptionInput).toBeEnabled();
+
+        const saveButton = screen.getByRole("button", {
+          name: /save changes/i,
+        });
+
+        expect(saveButton).toBeInTheDocument();
+        expect(saveButton).toBeEnabled();
+      });
+
+      const nameInput = screen.getByRole("textbox", {
+        name: /name:/i,
+      });
+      const descriptionInput = screen.getByRole("textbox", {
+        name: /description:/i,
+      });
+      const saveButton = screen.getByRole("button", {
+        name: /save changes/i,
+      });
+      await user.clear(descriptionInput);
+
+      await waitFor(() => {
+        //expect name and description to be populated
+        expect(nameInput).toBeInTheDocument();
+
+        expect(descriptionInput).toBeInTheDocument();
+        expect(descriptionInput).toHaveValue("");
+      });
+
+      await user.click(saveButton);
+
+      await waitFor(() => {
+        expect(addFinToast).toBeCalledWith(
+          "success",
+          "Team updated successfully."
+        );
+        expect(screen.getByText("It Redirected Yay!")).toBeInTheDocument();
+      });
+    });
   });
 });
 
@@ -81,6 +300,7 @@ const wrappedView = (teamId: string) => (
     <MemoryRouter initialEntries={[`/teams/${teamId}/edit`]}>
       <Routes>
         <Route path="/teams/:teamId/edit" element={<EditTeam />} />
+        <Route path="/teams/:teamId" element={<div>It Redirected Yay!</div>} />
       </Routes>
     </MemoryRouter>
   </QueryClientProvider>
--- Finjector.Web/ClientApp/test/mocks/handlers.ts
+++ Finjector.Web/ClientApp/test/mocks/handlers.ts
@@ -153,4 +153,20 @@ export const handlers = [
 
     return HttpResponse.json(fakeTeams[0]);
   }),
+  http.put("/api/team/:id", async ({ request }) => {
+    //console.log("request", request);
+
+    //get the body
+    const body = await request.json();
+    //console.log("body", body);
+    //get the name and description from the body source
+    const { name } = body as NameAndDescriptionModel;
+    //console.log("name", name);
+    if (name === "TEST ERROR") {
+      console.log("Returning error");
+      return HttpResponse.error();
+    }
+
+    return HttpResponse.json(fakeTeams[0]);
+  }),
 ];
